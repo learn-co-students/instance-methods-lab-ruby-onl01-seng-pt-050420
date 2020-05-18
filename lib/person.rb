000660db9326a3db6d98d23e4ebcaac39ec4bb65
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-18 01:59:43 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person  # starts defining our Person class
+  def talk  # start defining our #talk method
+    puts "Hello World!"   
+  end   
+end  
+
+class Person  # starts defining our Person class
+  def walk  # start defining our #talk method
+    puts "The Person is walking"   
+  end   
+end 
